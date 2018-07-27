886f2afa8aad4dc528d89b6633da7119fbed8a5b
Candice Schramm <candice@Candices-MacBook-Pro.local>
2018-07-26 20:55:26 -0600

    structure of files working, handlebars index not working yet

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('monster burger');
+INSERT INTO burgers (burger_name) VALUES ('nacho burger');
+INSERT INTO burgers (burger_name) VALUES ('spicy burger');
+
